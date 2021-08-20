0242e45c92641f337af9f74ffe6ac51f5e75c9c5
Edmund Cheung <edmundcheung1@gmail.com>
2021-08-20 14:00:50 +0100

    all models updated

---
+++ models/logistic_regression/README.md
@@ -0,0 +1,8 @@
+
+    ## Logistic regression models trained on original data
+    Contents:
+    - `log_base.p` - trained LogisicRegression instance (baseline classifier)
+    - `log_weight.p` - trained LogisicRegression instance (baseline classifier with class_weights='balanced')
+    - `log_gs.p` - trained LogisicRegression instance (best classifier)
+    - `data.p` - data tuple (X_train, X_test, y_train, y_test)
+    
--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,11 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, cross_val_score
 from imblearn.over_sampling import SMOTE
-from sklearn.metrics import plot_roc_curve as plot_roc_curve_sk
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import plot_roc_curve as plot_roc_curve_sk, classification_report
 
+from matplotlib import pyplot as plt
+from  matplotlib.colors import LinearSegmentedColormap
+import seaborn as sns
 from matplotlib import pyplot as plt
 
 def preprocess(df,
@@ -104,21 +106,114 @@ def plot_feature_importances(feature_importances, feature_names,
         return feature_importances[feature_importances < thresh]
 
 
-def plot_roc_curve(clfs, X, y):
-    fig, ax = plt.subplots()
+def plot_roc_curve(clfs, X, y, labels=None, shade=True,
+    subplots_kwargs={}, save_fpath=None):
+    fig, ax = plt.subplots(**subplots_kwargs)
 
     if isinstance(clfs, list):
-        for i, clf in enumerate(clfs):
-            y_test = y[i]
-            y_pred = clf.predict(X[i])
-            fpr, tpr, thresholds = roc_curve(y_test, y_pred)
-            auc_score = auc(fpr, tpr)
-            plot_roc_curve_sk(clf, X[i], y[i], ax=ax)#, label=f'Classifier {i+1}: AUC = {(auc_score).round(2)}')
+        if labels is not None:
+            assert isinstance(labels, list)
+        for clf, label in zip(clfs, labels):
+            displ = plot_roc_curve_sk(clf, X, y, ax=ax, name=label,
+                ls="--", alpha=0.8)
+            if shade:
+                ax.fill_between(x=displ.fpr, y1=displ.tpr,
+                    color=displ.line_.get_color(), alpha=0.1)
     else:
         plot_roc_curve_sk(clfs, X, y, ax=ax)
 
     xfit = np.linspace(0, 1, 1000)
-    ax.plot(xfit, xfit, "--", label="Skill-Less Classifier")
+    ax.plot(xfit, xfit, "--", label="Skill-Less Classifier (AUC=0.50)", alpha=0.8)
+    plt.legend(fontsize="x-large", loc='lower right')
+    plt.xlabel('False Positive Rate (Positive label: 1)')
+    plt.ylabel('True Positive Rate (Positive label: 1)')
+    if save_fpath is not None:
+        plt.savefig(save_fpath, bbox_inches="tight")
 
-    plt.legend()
     plt.show()
+
+def print_classification_reports(clfs, Xs, ys, names):
+    for clf, X, y, name in zip(clfs, Xs, ys, names):
+        print(f"{name}: {sep}")
+        print(classification_report(y, clf.predict(X)))
+
+
+def red_greens_cmap():
+    """
+    Red-to-green seaborn cmap
+    Ref:
+    https://stackoverflow.com/questions/38246559/how-to-create-a-heat-map-in-python-that-ranges-from-green-to-red
+    """
+    c = ["darkred", "red", "lightcoral", "white", "palegreen", "green", "darkgreen"]
+    v = [0,.15,.4,.5,0.6,.9,1.]
+    l = list(zip(v,c))
+    return LinearSegmentedColormap.from_list('rg',l, N=256)
+
+
+def classification_report_df(clf, X, y, clean=True,
+    plot=True, save_fpath=None):
+    df = pd.DataFrame(classification_report(y,clf.predict(X),
+            output_dict=True))
+
+    df.loc[:, "accuracy"] = [np.nan, np.nan, df.accuracy[0], df["macro avg"][-1]]
+    df = df.T
+    if clean:
+        df = df.round(2)
+        df.loc[:, "support"] = df.support.astype(int)
+
+    if plot:
+        sns.heatmap(df.drop("support", axis=1), cmap=red_greens_cmap(), annot=True)
+        if save_fpath is not None:
+            plt.savefig(save_fpath, bbox_inches="tight")
+        plt.show()
+    return df
+
+def compare_classification_reports(clfs, Xs, ys, names, clean=True,
+    plot=True, save_fpath=None, suptitle=None, subplot_kwargs={},
+    cbar_orient="horizontal"):
+    """
+    Compare classification reports for classifiers
+    Arguments:
+    - clfs - list of classifiers
+    - Xs - list of X dataframes (for each classifier)
+    - ys - list of known classifications (for each classifier)
+    - names - list of classifier names (for labelling)
+    - clean - round all floats to 2.d.p.
+    - plot - plot report comparison
+    - save_fpath - file path to save plot
+    - suptite - super title of figure
+    - subplot_kwargs - kwards for plt.suplots()
+    - cbar_orient - orientation of cbar (either 'horizontal' or 'vertical')
+    Returns:
+    - dfs - list of classification report dataframes
+    - plot - if plot==True, displays comparison plot
+    """
+    dfs = []
+    for clf, X, y in zip(clfs, Xs, ys):
+        dfs.append(classification_report_df(clf, X, y, clean=clean, plot=False))
+
+    vmin = min([df.drop("support", axis=1).min().min() for df in dfs])
+    vmax = max([df.drop("support", axis=1).max().max() for df in dfs])
+
+    fig, axs = plt.subplots(**subplot_kwargs, sharex=True, sharey=True)
+    cmap = red_greens_cmap()
+
+    if plot:
+        for df, ax, name, cbar in zip(dfs, axs, names, [False, True]):
+            im = sns.heatmap(df.drop("support", axis=1),
+                cmap=cmap, annot=True, vmin=vmin, vmax=vmax, cbar=False, ax=ax)
+            ax.set_title(name)
+
+        mappable = im.get_children()[0]
+        plt.colorbar(mappable, ax=axs, orientation = cbar_orient)
+
+        if suptitle is not None:
+            plt.suptitle(suptitle, fontsize="x-large")
+        # fig.tight_layout()
+
+        if save_fpath is not None:
+            plt.savefig(save_fpath, bbox_inches="tight", edgecolor='white')
+
+        plt.show()
+
+    return dfs
